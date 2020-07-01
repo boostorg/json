904252de0b19e4ce2b77e14ed250aea3dc6f861d
Krystian Stasiowski <sdkrystian@gmail.com>
2020-07-01 00:06:05 -0400

    Fix repo links
    close #123

--- test/make-pvs.py
+++ test/make-pvs.py
@@ -48,7 +48,7 @@ print("""
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Official repository: https://github.com/vinniefalco/json
+// Official repository: https://github.com/cppalliance/json
 //
 
 #ifndef PARSE_VECTORS
