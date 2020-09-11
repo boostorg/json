408ae164779f0eb3399eb2163e09adeaee32cbff
Vinnie Falco <vinnie.falco@gmail.com>
2020-09-11 07:49:13 -0700

    Test parse allocation discrepancy

--- test/parse.cpp
+++ test/parse.cpp
@@ -12,6 +12,9 @@
 
 #include <boost/json/serialize.hpp>
 
+#include <string>
+
+#include "test.hpp"
 #include "test_suite.hpp"
 
 BOOST_JSON_NS_BEGIN
@@ -47,9 +50,99 @@ public:
     }
 
     void
+    testMemoryUsage()
+    {
+        // string
+        {
+            std::string s(10000, '*');
+            s.insert(s.begin(), '\"');
+            s.append(1, '\"');
+
+            fail_resource mr1;
+            value jv1 = parse(s, &mr1);
+
+            fail_resource mr2;
+            value jv2(jv1, &mr2);
+            BOOST_TEST(mr1.bytes == mr2.bytes);
+
+            fail_resource mr3;
+            value jv3(&mr3);
+            jv3 = jv1;
+            BOOST_TEST(mr1.bytes == mr3.bytes);
+        }
+
+        // array
+        {
+            {
+                fail_resource mr1;
+                value jv1 = parse("[1]", &mr1);
+
+                fail_resource mr2;
+                value jv2(jv1, &mr2);
+                BOOST_TEST(mr1.bytes == mr2.bytes);
+
+                fail_resource mr3;
+                value jv3(&mr3);
+                jv3 = jv1;
+                BOOST_TEST(mr1.bytes == mr3.bytes);
+            }
+            {
+                std::string s;
+                s = "1,2,3,4";
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = s + "," + s;
+                s = "[[" + s + "]]";
+
+                fail_resource mr1;
+                value jv1 = parse(s, &mr1);
+
+                fail_resource mr2;
+                value jv2(jv1, &mr2);
+                BOOST_TEST(mr1.bytes == mr2.bytes);
+
+                fail_resource mr3;
+                value jv3(&mr3);
+                jv3 = jv1;
+                BOOST_TEST(mr1.bytes == mr3.bytes);
+            }
+        }
+
+        // object
+        {
+            std::string s;
+            s = "{\"0\":null";
+            for(int i = 1; i < 1000; ++i)
+                s +=
+                    ",\"" +
+                    std::to_string(i) +
+                    "\":null";
+            s.append("}");
+
+            fail_resource mr1;
+            value jv1 = parse(s, &mr1);
+
+            fail_resource mr2;
+            value jv2(jv1, &mr2);
+            BOOST_TEST(mr1.bytes == mr2.bytes);
+
+            fail_resource mr3;
+            value jv3(&mr3);
+            jv3 = jv1;
+            BOOST_TEST(mr1.bytes == mr3.bytes);
+        }
+    }
+
+    void
     run()
     {
         testParse();
+        testMemoryUsage();
     }
 };
 
