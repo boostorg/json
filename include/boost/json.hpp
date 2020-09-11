996c8958fe15b99b5feea3af087a47e0e67eef84
Vinnie Falco <vinnie.falco@gmail.com>
2020-09-11 12:10:17 -0700

    Add visit
    fix #207

--- include/boost/json.hpp
+++ include/boost/json.hpp
@@ -14,6 +14,7 @@
 
 #include <boost/json/array.hpp>
 #include <boost/json/error.hpp>
+#include <boost/json/fwd.hpp>
 #include <boost/json/kind.hpp>
 #include <boost/json/memory_resource.hpp>
 #include <boost/json/monotonic_resource.hpp>
@@ -21,8 +22,8 @@
 #include <boost/json/number_cast.hpp>
 #include <boost/json/object.hpp>
 #include <boost/json/parse.hpp>
-#include <boost/json/parser.hpp>
 #include <boost/json/parse_options.hpp>
+#include <boost/json/parser.hpp>
 #include <boost/json/pilfer.hpp>
 #include <boost/json/serialize.hpp>
 #include <boost/json/serializer.hpp>
@@ -30,12 +31,14 @@
 #include <boost/json/storage_ptr.hpp>
 #include <boost/json/stream_parser.hpp>
 #include <boost/json/string.hpp>
+#include <boost/json/string_view.hpp>
 #include <boost/json/system_error.hpp>
 #include <boost/json/value.hpp>
 #include <boost/json/value_from.hpp>
 #include <boost/json/value_ref.hpp>
 #include <boost/json/value_stack.hpp>
 #include <boost/json/value_to.hpp>
+#include <boost/json/visit.hpp>
 
 // Intentionally excluded
 //#include <boost/json/basic_parser.hpp>
