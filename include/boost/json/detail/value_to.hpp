566ddca326dd52248036363a0a25baa294c54a9e
Richard Hodges <hodges.r@gmail.com>
2020-11-17 07:08:03 +0100

    Fix value_to and value_from for MSVC-14.0:
    value_to and value_from was incorrectly deducing that std::string was
    "array-like" rather than "string-like", but only on msvc-14.0
    
    Original test:
    
    template<class T, typename std::enable_if<
        std::is_constructible<remove_cvref<T>, const char*, std::size_t>::value &&
        std::is_convertible<decltype(std::declval<T&>().data()), const char*>::value &&
        std::is_convertible<decltype(std::declval<T&>().size()), std::size_t>::value
    >::type* = nullptr>
    
    Which works for all compilers except msvc-14.0
    
    New test:
    
    template<class T, typename std::enable_if<
        std::is_constructible<remove_cvref<T>, const char*, std::size_t>::value &&
        std::is_convertible<decltype(std::declval<T&>().data()), const char*>::value &&
        std::is_integral<decltype(std::declval<T&>().size())>::value
    >::type* = nullptr>
    
    Note that each individual test works on all compilers. It seems to be
    the conjuction of tests that caused msvc-14 to trip up.

--- include/boost/json/detail/value_to.hpp
+++ include/boost/json/detail/value_to.hpp
@@ -100,11 +100,13 @@ tag_invoke(
 // Use generic conversion
 
 // string-like types
+// NOTE: original check for size used is_convertible but 
+// MSVC-140 selects wrong specialisation if used
 template<class T, typename std::enable_if<
     std::is_constructible<T, const char*, std::size_t>::value &&
-        std::is_convertible<decltype(std::declval<T&>().data()), const char*>::value && 
-    std::is_convertible<decltype(std::declval<T&>().size()),
-        std::size_t>::value>::type* = nullptr>
+    std::is_convertible<decltype(std::declval<T&>().data()), const char*>::value && 
+    std::is_integral<decltype(std::declval<T&>().size())>::value
+>::type* = nullptr>
 T
 value_to_generic(
     const value& jv,
