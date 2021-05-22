e62a95d6379f2cbeaee18f997e68bd5c90db0ef1
Dmitry Arkhipov <grisumbras@gmail.com>
2021-05-22 17:14:56 +0300

    Remove trailing whitespace

--- include/boost/json/value.hpp
+++ include/boost/json/value.hpp
@@ -3924,8 +3924,8 @@ struct tuple_element<1, ::boost::json::key_value_pair const>
 namespace std {
 template <>
 struct hash< ::boost::json::value > {
-    BOOST_JSON_DECL 
-    std::size_t 
+    BOOST_JSON_DECL
+    std::size_t
     operator()(::boost::json::value const& jv) const noexcept;
 };
 } // std
