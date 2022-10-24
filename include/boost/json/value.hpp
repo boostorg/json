213726054b6f4c5b3e542045d31dd171db952c65
Dmitry Arkhipov <grisumbras@gmail.com>
2022-10-24 14:56:55 +0300

    fix duplicate word in value docs

--- include/boost/json/value.hpp
+++ include/boost/json/value.hpp
@@ -38,8 +38,7 @@ BOOST_JSON_NS_BEGIN
 /** The type used to represent any JSON value
 
     This is a
-    <a href="https://en.cppreference.com/w/cpp/concepts/regular"><em>Regular</em></a>.
-    <em>Regular</em>
+    <a href="https://en.cppreference.com/w/cpp/concepts/regular"><em>Regular</em></a>
     type which works like
     a variant of the basic JSON data types: array,
     object, string, number, boolean, and null.
