ccee5c541edc5bf95cbc1c301b1f911c34faeb3a
Dmitry Arkhipov <grisumbras@yandex.ru>
2023-10-10 21:55:54 +0300

    detail::stack places non-trivials and trivials in the same area

--- include/boost/json/detail/impl/stack.hpp
+++ include/boost/json/detail/impl/stack.hpp
@@ -10,79 +10,63 @@
 #ifndef BOOST_JSON_DETAIL_IMPL_STACK_HPP
 #define BOOST_JSON_DETAIL_IMPL_STACK_HPP
 
-#include <boost/core/max_align.hpp>
+#include <boost/align/align.hpp>
 #include <boost/static_assert.hpp>
-#include <new>
+#include <limits>
+#include <memory>
 
 namespace boost {
 namespace json {
 namespace detail {
 
-//--------------------------------------
-/*
-    We put the non-trivial objects at
-    the lo end of the buffer and the
-    trivial objects at the hi end:
-
-  base_        {free}     base_+cap_
-    |<------>|<------>|<------>|
-      size0_            size1_
-*/
-//--------------------------------------
-
 struct stack::non_trivial
 {
     non_trivial* next;
+    std::size_t offset;
 
     virtual BOOST_JSON_DECL ~non_trivial() = 0;
-    virtual non_trivial* copy(void*) noexcept = 0;
+    virtual non_trivial* relocate(void*) noexcept = 0;
     virtual void* get() = 0;
 };
 
 void
 stack::
-reserve_impl(
-    std::size_t n)
+reserve_impl(std::size_t n)
 {
     // caller checks this
     BOOST_ASSERT(n > cap_);
 
-    auto const base = static_cast<
-        unsigned char*>(sp_->allocate(n));
+    auto const base = static_cast<unsigned char*>( sp_->allocate(n) );
     if(base_)
     {
         // copy trivials
-        if(size1_ > 0)
-            std::memcpy(
-                base + n - size1_,
-                base_ + cap_ - size1_,
-                size1_);
+        std::memcpy(base, base_, size_);
 
         // copy non-trivials
-        if(head_)
+        non_trivial* src = head_;
+        non_trivial* prev = nullptr;
+        non_trivial* head = nullptr;
+        while(src)
         {
-            non_trivial* head = nullptr;
-            auto dest = reinterpret_cast<
-                non_trivial*>(base);
-            auto next = head_->next;
-            auto prev = dest;
-            dest = head_->copy(dest);
-            prev->next = head;
-            head = prev;
-            head_->~non_trivial();
-            head_ = next;
-            while(head_)
-            {
-                next = head_->next;
-                prev = dest;
-                dest = head_->copy(dest);
-                prev->next = head;
-                head = prev;
-                head_->~non_trivial();
-                head_ = next;
-            }
-            head_ = head;
+            auto const next = src->next;
+            auto const offset = src->offset;
+
+            std::size_t const buf_offset =
+                reinterpret_cast<unsigned char*>(src) - base_;
+            auto dest = reinterpret_cast<non_trivial*>(
+                src->relocate(base + buf_offset) );
+            dest->offset = offset;
+            dest->next = nullptr;
+
+            if( prev )
+                prev->next = dest;
+            else
+                head = dest;
+
+            prev = dest;
+            src = next;
         }
+        head_ = head;
 
         if(base_ != buf_)
             sp_->deallocate(base_, cap_);
@@ -96,15 +80,11 @@ void
 stack::
 push_unchecked(T const& t)
 {
+    constexpr std::size_t n = sizeof(T);
     BOOST_STATIC_ASSERT( is_trivially_copy_assignable<T>::value );
-    BOOST_ASSERT(
-        sizeof(T) <= cap_ - (
-            size0_ + size1_));
-    size1_ += sizeof(T);
-    std::memcpy(
-        base_ + cap_ - size1_,
-        &t,
-        sizeof(T));
+    BOOST_ASSERT( n <= cap_ - size_ );
+    std::memcpy( base_ + size_, &t, n );
+    size_ += n;
 }
 
 template<class T>
@@ -112,13 +92,10 @@ void
 stack::
 peek(T& t)
 {
+    constexpr std::size_t n = sizeof(T);
     BOOST_STATIC_ASSERT( is_trivially_copy_assignable<T>::value );
-    BOOST_ASSERT(
-        size1_ >= sizeof(T));
-    std::memcpy(
-        &t,
-        base_ + cap_ - size1_,
-        sizeof(T));
+    BOOST_ASSERT( size_ >= n );
+    std::memcpy( &t, base_ + size_ - n, n );
 }
 
 //--------------------------------------
@@ -129,12 +106,9 @@ void
 stack::
 push(T const& t, std::true_type)
 {
-    if(sizeof(T) > cap_ - (
-            size0_ + size1_))
-        reserve_impl(
-            size0_ +
-            sizeof(T) +
-            size1_);
+    if( sizeof(T) > cap_ - size_ )
+        reserve_impl( sizeof(T) + size_ );
+
     push_unchecked(t);
 }
 
@@ -142,12 +116,10 @@ push(T const& t, std::true_type)
 template<class T>
 void
 stack::
-push(
-    T const& t,
-    std::false_type)
+push(T const& t, std::false_type)
 {
-    struct alignas(core::max_align_t)
-        U : non_trivial
+    struct U
+        : non_trivial
     {
         T t_;
 
@@ -157,10 +129,11 @@ push(
         }
 
         non_trivial*
-        copy(void* dest) noexcept override
+        relocate(void* dest) noexcept override
         {
-            return (::new(dest)
-                U(t_)) + 1;
+            auto result = ::new(dest) U(t_);
+            this->~U();
+            return result;
         }
 
         void*
@@ -169,23 +142,37 @@ push(
             return &t_;
         }
     };
+
     BOOST_STATIC_ASSERT( ! is_trivially_copy_assignable<T>::value );
+
+    constexpr std::size_t n = sizeof(U);
+
+    void* ptr;
+    std::size_t offset;
+    do
+    {
+        std::size_t space = cap_ - size_;
+        unsigned char* buf = base_ + size_;
+        ptr = buf;
+        if( alignment::align(alignof(U), n, ptr, space) )
+        {
+            offset = (reinterpret_cast<unsigned char*>(ptr) - buf) + n;
+            break;
+        }
+
+        reserve_impl(size_ + n + alignof(U) - 1);
+    }
+    while(true);
     BOOST_ASSERT(
-        alignof(U) <= alignof(
-            core::max_align_t));
-    BOOST_ASSERT((sizeof(U) % alignof(
-        core::max_align_t)) == 0);
-    if(sizeof(U) > cap_ - (
-            size0_ + size1_))
-        reserve_impl(
-            size0_ +
-            sizeof(U) +
-            size1_);
-    auto nt = ::new(
-        base_ + size0_) U(t);
+        (reinterpret_cast<unsigned char*>(ptr) + n - offset) ==
+        (base_ + size_) );
+
+    auto nt = ::new(ptr) U(t);
     nt->next = head_;
+    nt->offset = offset;
+
     head_ = nt;
-    size0_ += sizeof(U);
+    size_ += offset;
 }
 
 // trivial
@@ -194,10 +181,9 @@ void
 stack::
 pop(T& t, std::true_type)
 {
-    BOOST_ASSERT(
-        size1_ >= sizeof(T));
+    BOOST_ASSERT( size_ >= sizeof(T) );
     peek(t);
-    size1_ -= sizeof(T);
+    size_ -= sizeof(T);
 }
 
 // non-trivial
@@ -206,17 +192,14 @@ void
 stack::
 pop(T& t, std::false_type)
 {
-    t = std::move(*reinterpret_cast<
-        T*>(head_->get()));
     auto next = head_->next;
+    auto offset = head_->offset;
+
+    t = std::move( *reinterpret_cast<T*>( head_->get() ) );
     head_->~non_trivial();
+
     head_ = next;
-    if(head_)
-        size0_ = reinterpret_cast<
-            unsigned char const*>(head_) -
-                base_;
-    else
-        size0_ = 0;
+    size_ -= offset;
 }
 
 void
@@ -229,8 +212,7 @@ clear() noexcept
         head_->~non_trivial();
         head_ = next;
     }
-    size0_ = 0;
-    size1_ = 0;
+    size_ = 0;
 }
 
 stack::
@@ -238,8 +220,7 @@ stack::
 {
     clear();
     if(base_ != buf_)
-        sp_->deallocate(
-            base_, cap_);
+        sp_->deallocate(base_, cap_);
 }
 
 stack::
