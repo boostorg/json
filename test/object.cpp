b7457fe41db6cad640c388635437bbe7acee57c8
Ramya Eliger <ramya@digiscrypt.com>
2026-08-18 16:26:05 +0530

    check moved buffer identity via data pointer in insert_or_assign test

--- test/object.cpp
+++ test/object.cpp
@@ -1208,9 +1208,10 @@ public:
                     { "k1", 1 },
                     { "k2", { 1, 2, 3 } } };
                 value v = { 4, 5, 6 };
+                auto const d = v.as_array().data();
                 o.insert_or_assign("k2", std::move(v));
                 BOOST_TEST(o.at("k2") == (array{4, 5, 6}));
-                BOOST_TEST(v.as_array().empty());
+                BOOST_TEST(o.at("k2").as_array().data() == d);
             }
         }
 
